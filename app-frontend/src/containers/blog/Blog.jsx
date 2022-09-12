5ef310802789e8840c605e82173876edc9e345e2
SachKar <88773679+sachham-karki@users.noreply.github.com>
2022-09-12 17:54:19 +1000

    adding blog section

--- app-frontend/src/containers/blog/Blog.jsx
+++ app-frontend/src/containers/blog/Blog.jsx
@@ -1,8 +1,49 @@
 import React from "react";
+import { Article } from "../../components";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
 import "./blog.css";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="gpt3__blog section__padding " id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article
+            imgUrl={blog01}
+            date="Sep 09, 2022"
+            title="Anti-Matter and Code Automation. Let us explore how it is?"
+          />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article
+            imgUrl={blog02}
+            date="Sep 09, 2022"
+            title="Anti-Matter and AI is the future. Let us explore how it is?"
+          />{" "}
+          <Article
+            imgUrl={blog03}
+            date="Sep 09, 2022"
+            title="Anti-Matter suggesting  AI. Let us explore how it is?"
+          />{" "}
+          <Article
+            imgUrl={blog04}
+            date="Sep 09, 2022"
+            title="Anti-Matter and Block-Chain can bring new revolution. Let us explore how it is?"
+          />{" "}
+          <Article
+            imgUrl={blog05}
+            date="Sep 09, 2022"
+            title="Anti-Matter and Open  AI is the future. Let us explore how it is?"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
--- app-frontend/src/containers/possibility/Possibility.jsx
+++ app-frontend/src/containers/possibility/Possibility.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./possibility.css";
+import possibilityImage from "../../assets/possibility.png";
 
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <div className="gpt3__possibility section__padding" id="services">
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="possibility img" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>
+          We are here to digitalization of your business, and to scale-up with
+          modern technology. Our services AI, Block-Chain and all kind IT
+          counselling solution Web-App development, upgrade and Maintanance,
+          cloud services, Logo Design and Digital-Marketing.
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;
